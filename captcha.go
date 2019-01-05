86367d9fd04d36424b390fc94ea02f6c82e0fb91
Kitisak.s <kitisak.s@linecorp.com>
2019-01-05 14:48:42 +0700

    refactor: use switch case && map NumberToWord

--- captcha.go
+++ captcha.go
@@ -1,11 +1,25 @@
 package captcha
 
+import "fmt"
+
+var NumberToWord = map[int]string{
+	1: "one",
+	2: "two",
+	3: "three",
+	4: "four",
+	5: "five",
+	6: "six",
+	7: "seven",
+	8: "eight",
+	9: "nine",
+}
+
 func Captcha(p, n1, op, n2 int) string {
-	if op == 3 {
-		return "2 x one"
+	switch op {
+	case 1:
+		return fmt.Sprintf("%v + %v", n1, NumberToWord[n2])
+	case 2:
+		return fmt.Sprintf("%v - %v", n1, NumberToWord[n2])
 	}
-	if n1 == 4 {
-		return "4 - two"
-	}
-	return "5 + two"
+	return fmt.Sprintf("%v x %v", n1, NumberToWord[n2])
 }
